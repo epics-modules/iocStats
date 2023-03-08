cc132d563e817ca8f036e004b6e9229ce51a4c32
Simon Rose <simon.rose@ess.eu>
2023-03-08 14:16:11 +0100

    Fix RTEMS 4.10 rtemsReboot call

--- devIocStats/os/RTEMS/devIocStatsOSD.h
+++ devIocStats/os/RTEMS/devIocStatsOSD.h
@@ -68,6 +68,10 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include "epicsVersion.h"
+
+#define RTEMS_VERSION_INT VERSION_INT(__RTEMS_MAJOR__, __RTEMS_MINOR__, __RTEMS_REVISION__, 0)
+
 #define sysBootLine rtems_bsdnet_bootp_cmdline
 /* Override default STARTUP environment variable to use INIT */
 #undef  STARTUP
@@ -76,10 +80,10 @@
 
 #ifdef RTEMS_BSP_PGM_EXEC_AFTER /* only defined on uC5282 */
 #define reboot(x) bsp_reset(0)
-#elif   (defined(__PPC__) && ((__RTEMS_MAJOR__ > 4) \
-         || (__RTEMS_MAJOR__ == 4 && __RTEMS_MINOR__ > 9) \
-         || (__RTEMS_MAJOR__ == 4 && __RTEMS_MINOR__ == 9 && __RTEMS_REVISION__ > 0)))
+#elif   (defined(__PPC__) && RTEMS_VERSION_INT > VERSION_INT(4,9,0,0))
 #define reboot(x) bsp_reset()
+#elif   (defined(__i386__) && RTEMS_VERSION_INT >= VERSION_INT(4,10,0,0))
+#define reboot(x) { void bsp_reset(); bsp_reset();}
 #else
 #define reboot(x) rtemsReboot()
 #endif
