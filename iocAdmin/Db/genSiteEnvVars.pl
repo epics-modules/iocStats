3b07e0fc2c1c912b87178e5c74653407ac9bfe5d
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2025-06-10 00:20:35 +0100

    Tidy up perl script to use here doc

--- iocAdmin/Db/genSiteEnvVars.pl
+++ iocAdmin/Db/genSiteEnvVars.pl
@@ -1,7 +1,9 @@
-print("file iocEnvVar.template\n");
-print("{\n");
-print("pattern\n");
-print("{ ENVNAME, ENVVAR, ENVTYPE }\n");
+#!/usr/bin/env perl
+print <<__END__;
+file "iocEnvVar.template" {
+  pattern { ENVNAME, ENVVAR, ENVTYPE }
+__END__
+
 while(<>) {
     $m = s/^EPICS_([A-Z_]*).*/{${1}, EPICS_${1}, epics}/;
     print $_ if $m;
