b53dbfd90f5de8ff54bd11b0265c0233edb745a4
Simon Rose <simon.rose@ess.eu>
2025-12-18 09:34:36 +0100

    Unique-ify the output of genSiteEnvVars.pl

--- iocAdmin/Db/genSiteEnvVars.pl
+++ iocAdmin/Db/genSiteEnvVars.pl
@@ -1,18 +1,29 @@
 #!/usr/bin/env perl
 #
-# Usage:  genSiteEnvVars.pl CONFIG_SITE_ENV
+# Usage:  genSiteEnvVars.pl CONFIG_SITE_ENV [ ... ]
 #
 # Reads an EPICS CONFIG_SITE_ENV file and outputs
 # a substitutions file to generate appropriate EPICS
 # records for those variables via iocEnvVar.template
 #
+use strict;
+use warnings;
+
 print <<__END__;
 file "iocEnvVar.template" {
   pattern { ENVNAME, ENVVAR, ENVTYPE }
 __END__
 
+my @epics_vars;
 while(<>) {
-    $m = s/^EPICS_([A-Z_]*).*/{${1}, EPICS_${1}, epics}/;
-    print $_ if $m;
+    my $m = s/^EPICS_([A-Z_0-9]*).*\n/${1}/;
+    push(@epics_vars, $_) if $m;
+}
+
+my %varcount;
+my @unique_vars = grep { not $varcount{$_}++ } @epics_vars;
+
+foreach (@unique_vars) {
+    print "{ $_, EPICS_$_, epics }\n"
 }
 print("}\n");
